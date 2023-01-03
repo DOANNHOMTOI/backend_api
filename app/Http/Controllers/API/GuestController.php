369687cb586c07a0b9c3478f95e345c6e9a3324d
longpv <longpv@rikkeisoft.com>
2023-01-03 08:20:30 +0700

    ok

--- app/Http/Controllers/API/GuestController.php
+++ app/Http/Controllers/API/GuestController.php
@@ -14,17 +14,18 @@ use Illuminate\Support\Facades\Validator;
 
 class GuestController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $limit = $request->page = -1 ? 1000000 : env('APP_LIMIT_PAGE');
-        return $this->sendResponse(Guest::orderBy('created_at','DESC')->paginate($limit),'success');
+        return $this->sendResponse(Guest::orderBy('created_at', 'DESC')->paginate($limit), 'success');
     }
     public function store(Request $request)
     {
-        $guestPhone = Guest::where('phone',$request->phone)->first();
-        $guestEmail = Guest::where('email',$request->email)->first();
+        $guestPhone = Guest::where('phone', $request->phone)->first();
+        $guestEmail = Guest::where('email', $request->email)->first();
 
-        if ($guestPhone != null ||$guestEmail !=null ){
-            return $this->sendError('Số điện thoại hoặc email đã đăng ký tài khoản khác','Số điện thoại đã đăng ký tài khoản khác');
+        if ($guestPhone != null || $guestEmail != null) {
+            return $this->sendError('Số điện thoại hoặc email đã đăng ký tài khoản khác', 'Số điện thoại đã đăng ký tài khoản khác');
         }
         $guest = new Guest();
         $guest->name = trim($request->name);
@@ -39,24 +40,25 @@ class GuestController extends Controller
     }
     public function login(Request $request)
     {
-        $guest = Guest::where('phone',$request->phone)->where('password',$request->password)->where('status',1)->first();
+        $guest = Guest::where('phone', $request->phone)->where('password', $request->password)->where('status', 1)->first();
         return $this->sendResponse($guest, 'success');
     }
 
-    public function forgetPass(Request $request){
+    public function forgetPass(Request $request)
+    {
 
         $validator = Validator::make($request->all(), [
             'email' => 'required',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
 
         $newPass = rand(100000, 999999);
         $guest = Guest::where('email', $request->email)->first();
-        if (empty($guest)){
-            return $this->sendError(null,'Không tìm thấy tài khoản nào');
+        if (empty($guest)) {
+            return $this->sendError(null, 'Không tìm thấy tài khoản nào');
         }
         $guest->update(
             ['password' => $newPass]
@@ -68,5 +70,20 @@ class GuestController extends Controller
         return response()->json([
             'message' => 'Mật khẩu mới đã gửi về email của bạn',
         ], 200);
+    }
+
+    public function updateGuest(Request $request)
+    {
+        $guest = Guest::where('email', $request->email)->first();
+
+        if (empty($guest)) {
+            return $this->sendError(null, 'Không tìm thấy tài khoản nào');
+        }
+        $guest->name = $request->name;
+        $guest->phone = $request->phone;
+        $guest->address = $request->address;
+        $guest->password = $request->password;
+        $guest->save();
+        return $this->sendResponse($guest, 'success');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,67 +30,67 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // API ADMIN
-    Route::post('/validateToken', [UserController::class, 'validateToken']);
+Route::post('/validateToken', [UserController::class, 'validateToken']);
 
-    Route::post('/changePassWord', [UserController::class, 'changePassWord']);
+Route::post('/changePassWord', [UserController::class, 'changePassWord']);
 
-    Route::post('/upload', [\App\Http\Controllers\API\UploadFileController::class, 'upload']);
+Route::post('/upload', [\App\Http\Controllers\API\UploadFileController::class, 'upload']);
 
-    Route::get('/product-category', [\App\Http\Controllers\API\ProductCategoryController::class, 'index']);
-    Route::get('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'detail']);
-    Route::post('/product-category', [\App\Http\Controllers\API\ProductCategoryController::class, 'store']);
-    Route::put('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'update']);
-    Route::delete('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'delete']);
+Route::get('/product-category', [\App\Http\Controllers\API\ProductCategoryController::class, 'index']);
+Route::get('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'detail']);
+Route::post('/product-category', [\App\Http\Controllers\API\ProductCategoryController::class, 'store']);
+Route::put('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'update']);
+Route::delete('/product-category/{id}', [\App\Http\Controllers\API\ProductCategoryController::class, 'delete']);
 
-    Route::get('/voucher', [\App\Http\Controllers\API\VoucherController::class, 'index']);
-    Route::get('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'detail']);
-    Route::post('/voucher', [\App\Http\Controllers\API\VoucherController::class, 'store']);
-    Route::put('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'update']);
-    Route::delete('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'delete']);
+Route::get('/voucher', [\App\Http\Controllers\API\VoucherController::class, 'index']);
+Route::get('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'detail']);
+Route::post('/voucher', [\App\Http\Controllers\API\VoucherController::class, 'store']);
+Route::put('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'update']);
+Route::delete('/voucher/{id}', [\App\Http\Controllers\API\VoucherController::class, 'delete']);
 
-    Route::get('/product', [\App\Http\Controllers\API\ProductController::class, 'index']);
-    Route::get('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'detail']);
-    Route::post('/product', [\App\Http\Controllers\API\ProductController::class, 'store']);
-    Route::put('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'update']);
-    Route::delete('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'delete']);
+Route::get('/product', [\App\Http\Controllers\API\ProductController::class, 'index']);
+Route::get('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'detail']);
+Route::post('/product', [\App\Http\Controllers\API\ProductController::class, 'store']);
+Route::put('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'update']);
+Route::delete('/product/{id}', [\App\Http\Controllers\API\ProductController::class, 'delete']);
 
-    Route::get('/permission', [\App\Http\Controllers\API\PermissionController::class, 'index']);
-    Route::get('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'detail']);
-    Route::post('/permission', [\App\Http\Controllers\API\PermissionController::class, 'store']);
-    Route::put('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'update']);
-    Route::delete('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'delete']);
+Route::get('/permission', [\App\Http\Controllers\API\PermissionController::class, 'index']);
+Route::get('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'detail']);
+Route::post('/permission', [\App\Http\Controllers\API\PermissionController::class, 'store']);
+Route::put('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'update']);
+Route::delete('/permission/{id}', [\App\Http\Controllers\API\PermissionController::class, 'delete']);
 
-    Route::post('/permission-assign', [\App\Http\Controllers\API\PermissionController::class, 'assign']);
-    Route::get('/permissionByUser/{id}', [\App\Http\Controllers\API\PermissionController::class, 'permissionByUser']);
+Route::post('/permission-assign', [\App\Http\Controllers\API\PermissionController::class, 'assign']);
+Route::get('/permissionByUser/{id}', [\App\Http\Controllers\API\PermissionController::class, 'permissionByUser']);
 
-    Route::get('/customer', [\App\Http\Controllers\API\CustomerController::class, 'index']);
+Route::get('/customer', [\App\Http\Controllers\API\CustomerController::class, 'index']);
 
-    Route::get('/guest', [\App\Http\Controllers\API\GuestController::class, 'index']);
+Route::get('/guest', [\App\Http\Controllers\API\GuestController::class, 'index']);
 
-    Route::get('/rating', [\App\Http\Controllers\API\RatingController::class, 'index']);
-    Route::put('/rating/{id}', [\App\Http\Controllers\API\RatingController::class, 'update']);
+Route::get('/rating', [\App\Http\Controllers\API\RatingController::class, 'index']);
+Route::put('/rating/{id}', [\App\Http\Controllers\API\RatingController::class, 'update']);
 
 
-    Route::get('/order', [\App\Http\Controllers\API\OrderController::class, 'index']);
-    Route::get('/order/{id}', [\App\Http\Controllers\API\OrderController::class, 'detail']);
-    Route::put('/order/{id}', [\App\Http\Controllers\API\OrderController::class, 'update']);
+Route::get('/order', [\App\Http\Controllers\API\OrderController::class, 'index']);
+Route::get('/order/{id}', [\App\Http\Controllers\API\OrderController::class, 'detail']);
+Route::put('/order/{id}', [\App\Http\Controllers\API\OrderController::class, 'update']);
 
-    Route::get('/banner', [\App\Http\Controllers\API\BannerController::class, 'index']);
-    Route::post('/banner', [\App\Http\Controllers\API\BannerController::class, 'store']);
-    Route::get('/banner/{id}', [\App\Http\Controllers\API\BannerController::class, 'detail']);
-    Route::put('/banner/{id}', [\App\Http\Controllers\API\BannerController::class, 'update']);
+Route::get('/banner', [\App\Http\Controllers\API\BannerController::class, 'index']);
+Route::post('/banner', [\App\Http\Controllers\API\BannerController::class, 'store']);
+Route::get('/banner/{id}', [\App\Http\Controllers\API\BannerController::class, 'detail']);
+Route::put('/banner/{id}', [\App\Http\Controllers\API\BannerController::class, 'update']);
 
-    Route::get('/dash', [\App\Http\Controllers\API\DashController::class, 'index']);
+Route::get('/dash', [\App\Http\Controllers\API\DashController::class, 'index']);
 
-    Route::get('/report/order', [\App\Http\Controllers\API\ReportController::class, 'order']);
+Route::get('/report/order', [\App\Http\Controllers\API\ReportController::class, 'order']);
 
-    Route::get('/report/guest', [\App\Http\Controllers\API\ReportController::class, 'guest']);
+Route::get('/report/guest', [\App\Http\Controllers\API\ReportController::class, 'guest']);
 
-    Route::get('/partner', [\App\Http\Controllers\API\PartnerController::class, 'index']);
-    Route::get('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'detail']);
-    Route::post('/partner', [\App\Http\Controllers\API\PartnerController::class, 'store']);
-    Route::put('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'update']);
-    Route::delete('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'delete']);
+Route::get('/partner', [\App\Http\Controllers\API\PartnerController::class, 'index']);
+Route::get('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'detail']);
+Route::post('/partner', [\App\Http\Controllers\API\PartnerController::class, 'store']);
+Route::put('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'update']);
+Route::delete('/partner/{id}', [\App\Http\Controllers\API\PartnerController::class, 'delete']);
 
 Route::group(['prefix' => 'web'], function () {
     Route::get('/product-category', [\App\Http\Controllers\API\ProductCategoryController::class, 'index']);
@@ -112,3 +112,4 @@ Route::group(['prefix' => 'web'], function () {
 });
 Route::post('/changePassWord', [UserController::class, 'changePassWord']);
 Route::get('/orderHistory', [\App\Http\Controllers\API\OrderController::class, 'orderHistory']);
+Route::post('/update', [GuestController::class, 'updateGuest']);
